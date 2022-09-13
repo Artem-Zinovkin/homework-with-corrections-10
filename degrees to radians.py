0683498a183e1361a566ba0a1f3776bb0980009f
Artem-Zinovkin <113043587+Artem-Zinovkin@users.noreply.github.com>
2022-09-13 17:14:57 +0300

    Add files via upload

---
+++ degrees to radians.py
@@ -0,0 +1,10 @@
+# конвертация градусов в радианы
+import math
+
+
+def degrees(a):
+    return round((a * math.pi) / 180, 6)
+
+
+b = degrees(a=float(input("Введите число в градусах ")))
+print(F"число в радианах = {b} ")
